8714f5cf0699258b122b06337137dc40d43a44a1
Mac <mac@Macde-MacBook-Pro.local>
2020-04-11 11:51:37 +0800

    while >0

---
+++ pwd2.py
@@ -0,0 +1,19 @@
+#密碼重試程式
+#密碼是＝a123456
+#最多輸入三次密碼
+#錯誤會印出 ”密碼錯誤，還剩ＸＸ次幾會“ 對的話印出 “登入成功”
+
+password = 'a123456'
+c = 3 #剩餘機會
+
+while c > 0  :
+	pwd = input('please enter your password :')
+	if pwd == password :
+		print('successful logined')
+		break
+	else :
+		c = c - 1
+		print('wrong! still have', c , 'times !')
+		if c == 0 :
+			print('no chance!')
+			
